dadbaec1631f0760178704320e2c02eda2439d39
Krystopher88 <bichon.christ@gmail.com>
2023-07-24 17:10:43 +0200

    first commit

---
+++ lib/config.php
@@ -0,0 +1,12 @@
+<?php
+define("_RECIPES_IMG_PATH_", "./uploads/recipes/");
+define("_ASSETS_IMG_PATH_", "./assets/images/");
+define("_home_recipes_limit_", 3);
+
+
+
+$mainMenu = [
+  'index.php' => 'Accueil',
+  'recettes.php' => 'Nos recettes',
+  'ajout_modification_recette.php' => 'Gestion des recettes'
+];
